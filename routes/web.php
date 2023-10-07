f1ce041762b40ae12e607df7afe78bc2b6f33e9a
patrickpcribeiro <patrick.coimbra.ribeiro@gmail.com>
2023-10-07 17:52:58 -0300

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,27 @@
 
 use App\Http\Controllers\Data\{PoliticController};
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/contact', [SiteController::class, 'contact'])->name('site.contact');
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/forum', [PoliticController::class, 'forum'])->name('forum.index');
+
+Route::get('/suport', [PoliticController::class, 'suport'])->name('suport.index');
+
 
 Route::get('/politics', [PoliticController::class, 'politics'])->name('politics.index');
 

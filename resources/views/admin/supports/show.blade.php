c7abebb31315b3ee124fc011b0896cf6a939a04c
patrickpcribeiro <patrick.coimbra.ribeiro@gmail.com>
2023-10-10 19:53:58 -0300

    create view show supports

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -1,12 +1,20 @@
 <h1>Detalhes</h1>
 
 <ul>
-    <li> Id: {{ $support->id }}</li>
-    <li> Assunto: {{ $support->subject }}</li>
-    <li> Status: {{ $support->status }}</li>
-    <li> Comentário: {{ $support->body }}</li>
-    <li> Data da criação: {{ $support->created_at }}</li>
-    <li> Data da última atualização: {{ $support->updated_at }}</li>
+    <li> Id: {{ $supports->id }}</li>
+    <li> Assunto: {{ $supports->subject }}</li>
+    <li> Status: {{ $supports->status }}</li>
+    <li> Comentário: {{ $supports->body }}</li>
+    <li> Data da criação: {{ $supports->created_at }}</li>
+    <li> Data da última atualização: {{ $supports->updated_at }}</li>
 
+    <a class="btn btn-primary btn-sm" href="{{ route('supports.edit', $supports->id) }}" role="button">Edit</a>
+    {{-- <a class="btn btn-primary btn-sm" href="{{ route('supports.destroy', $supports->id) }}" role="button">Delete</a> --}}
+
+    <form action="{{ route('supports.destroy', $supports->id) }}" method="post">
+        @csrf()
+        @method('DELETE')
+        <button type="submit">Delete</button>
+    </form>
     
 </ul>

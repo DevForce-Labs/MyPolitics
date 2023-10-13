36362d8ea607727cce1fd19d36a3be9506894159
patrickpcribeiro <patrick.coimbra.ribeiro@gmail.com>
2023-10-13 08:04:37 -0300

    UPDT

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -4,13 +4,30 @@
 
 use App\DTO\{CreateSupportDTO};
 use App\DTO\UpdateSupportDTO;
+use App\Models\Support;
 use App\Repositories\SupportRepositoryInterface;
 
 class SupportEloquentORM implements SupportRepositoryInterface
 {
+    public function __construct(
+        protected Support $model
+    ){}
+
     public function getAll(string $filter = null): array
     {
+        return $this->model
+                    ->where(function ($query) use ($filter) { 
+                        if($filter) {
+                            $query->where('subject', $filter);
+                            $query->orWhere('body', 'like', "%{$filter}%");
 
+                        }
+                    })
+                    // ->paginate()
+                    ->all()
+                    ->toArray();
+
+                    // 6.30 aula 22
     }
 
     public function findOne(string $id): stdClass|null
